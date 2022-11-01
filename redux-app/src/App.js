a45d7e17c111287225c288c4a0beeefa6bd72408
KimSoungMin1 <tjdals247470@naver.com>
2022-11-01 17:47:59 +0900

    리덕스

---
+++ redux-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import CountBox from './components/CountBox';
+import Memo from './components/Memo';
+
+function App() {
+  return (
+    <div className="App">
+      <CountBox/>
+      <Memo/>
+    </div>
+  );
+}
+
+export default App;
